09302c1b0e1a41a1824c382097364df6745e73c6
KevinSC <kevineitor22@gmail.com>
2015-12-04 11:52:51 -0500

    un hola mundo mas decente

--- pybuk.r.py
+++ pybuk.r.py
@@ -1,11 +1,18 @@
 import urwid
 
 
-def show_or_exit(key):
-	if key in ('q', 'Q'):
-		raise urwid.ExitMainLoop()
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
 
-txt = urwid.Text("Hello World")
-fill = urwid.Filler(txt, 'top')
-loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+palette = [
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark red'),
+    ('bg', 'black', 'dark blue')]
+
+txt = urwid.Text(('banner', 'Hello\nWorldñfdfdfdfdf'), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
 loop.run()
