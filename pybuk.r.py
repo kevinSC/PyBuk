276476af27749ad9c3c4c475daa131c9f19238fc
KevinSC <kevineitor22@gmail.com>
2015-12-05 17:44:11 -0500

    menu secuencial

--- pybuk.r.py
+++ pybuk.r.py
@@ -1,81 +1,90 @@
 import urwid
 
 
-def menu_button(caption, callback):
-    button = urwid.Button(caption)
-    urwid.connect_signal(button, 'click', callback)
-    return urwid.AttrMap(button, None, focus_map='reversed')
+class MenuButton(urwid.Button):
+    def __init__(self, caption, callback):
+        super(MenuButton, self).__init__("")
+        urwid.connect_signal(self, 'click', callback)
+        self._w = urwid.AttrMap(urwid.SelectableIcon(
+            [u'  \N{BULLET} ', caption], 2), None, 'selected')
 
 
-def sub_menu(caption, choices):
-    contents = menu(caption, choices)
+class SubMenu(urwid.WidgetWrap):
+    def __init__(self, caption, choices):
+        super(SubMenu, self).__init__(MenuButton(
+            [caption, u"\N{HORIZONTAL ELLIPSIS}"], self.open_menu))
+        line = urwid.Divider(u'\N{LOWER ONE QUARTER BLOCK}')
+        listbox = urwid.ListBox(urwid.SimpleFocusListWalker([
+            urwid.AttrMap(urwid.Text([u"\n  ", caption]), 'heading'),
+            urwid.AttrMap(line, 'line'),
+            urwid.Divider()] + choices + [urwid.Divider()]))
+        self.menu = urwid.AttrMap(listbox, 'options')
 
-    def open_menu(button):
-        return top.open_box(contents)
-    return menu_button([caption, u'...'], open_menu)
-
-
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    body.extend(choices)
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-
-def item_chosen(button):
-    response = urwid.Text([u'You chose ', button.label, u'\n'])
-    done = menu_button(u'Ok', exit_program)
-    top.open_box(urwid.Filler(urwid.Pile([response, done])))
+    def open_menu(self, button):
+        top.open_box(self.menu)
 
 
-def exit_program(button):
+class Choice(urwid.WidgetWrap):
+    def __init__(self, caption):
+        super(Choice, self).__init__(
+            MenuButton(caption, self.item_chosen))
+        self.caption = caption
+
+    def item_chosen(self, button):
+        response = urwid.Text([u'  You chose ', self.caption, u'\n'])
+        done = MenuButton(u'Ok', exit_program)
+        response_box = urwid.Filler(urwid.Pile([response, done]))
+        top.open_box(urwid.AttrMap(response_box, 'options'))
+
+
+def exit_program(key):
     raise urwid.ExitMainLoop()
 
-menu_top = menu(u'Main Menu', [
-    sub_menu(u'Applications', [
-        sub_menu(u'Accessories', [
-            menu_button(u'Text Editor', item_chosen),
-            menu_button(u'Terminal', item_chosen),
-        ]),
-    ]),
-    sub_menu(u'System', [
-        sub_menu(u'Preferences', [
-            menu_button(u'Appearance', item_chosen),
-        ]),
-        menu_button(u'Lock Screen', item_chosen),
-    ]),
-])
+menu_top = SubMenu(
+    u'Main Menu',
+    [SubMenu(
+        u'Applications',
+        [
+            SubMenu(
+                u'Accessories',
+                [
+                    Choice(u'Text Editor'),
+                    Choice(u'Terminal')])]),
+        SubMenu(
+            u'System',
+            [
+                SubMenu(
+                    u'Preferences',
+                    [Choice(u'Appearance')]),
+                Choice(u'Lock Screen')])])
+
+palette = [
+    (None, 'light gray', 'black'),
+    ('heading', 'black', 'light gray'),
+    ('line', 'black', 'light gray'),
+    ('options', 'dark gray', 'black'),
+    ('focus heading', 'white', 'dark red'),
+    ('focus line', 'black', 'dark red'),
+    ('focus options', 'black', 'light gray'),
+    ('selected', 'white', 'dark blue')]
+focus_map = {
+    'heading': 'focus heading',
+    'options': 'focus options',
+    'line': 'focus line'}
 
 
-class CascadingBoxes(urwid.WidgetPlaceholder):
-    max_box_levels = 4
-
-    def __init__(self, box):
-        super(CascadingBoxes, self).__init__(urwid.SolidFill(u'/'))
-        self.box_level = 0
-        self.open_box(box)
+class HorizontalBoxes(urwid.Columns):
+    def __init__(self):
+        super(HorizontalBoxes, self).__init__([], dividechars=1)
 
     def open_box(self, box):
-        self.original_widget = urwid.Overlay(
-            urwid.LineBox(box),
-            self.original_widget,
-            align='center',
-            width=('relative', 80),
-            valign='middle',
-            height=('relative', 80),
-            min_width=24,
-            min_height=8,
-            left=self.box_level * 3,
-            right=(self.max_box_levels - self.box_level - 1) * 3,
-            top=self.box_level * 2,
-            bottom=(self.max_box_levels - self.box_level - 1) * 2)
-        self.box_level += 1
+        if self.contents:
+            del self.contents[self.focus_position + 1:]
+        self.contents.append((
+            urwid.AttrMap(box, 'options', focus_map),
+            self.options('given', 24)))
+        self.focus_position = len(self.contents) - 1
 
-    def keypress(self, size, key):
-        if key == 'esc' and self.box_level > 1:
-            self.original_widget = self.original_widget[0]
-            self.box_level -= 1
-        else:
-            return super(CascadingBoxes, self).keypress(size, key)
-
-top = CascadingBoxes(menu_top)
-urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
+top = HorizontalBoxes()
+top.open_box(menu_top.menu)
+urwid.MainLoop(urwid.Filler(top, 'middle', 10), palette).run()
